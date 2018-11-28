6e841f2a63a00e49f16350e646f1ecd40e6c3cf3
Steven Olson <steveoscaro@gmail.com>
2018-11-28 16:32:51 -0700

    refactors existing seed files and adds code to create Widgets too

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,17 @@
+# There are gems for this, but figured I'd just make a method here
+def create_random_string
+  ('a'..'z').to_a.shuffle[0,8].join
+end
+
 # Creating non-shipped orders.
-o1 = Order.create!()
-o2 = Order.create!()
-o3 = Order.create!()
-o4 = Order.create!()
-o5 = Order.create!()
+puts '***** Creating non-shipped Orders *****'
+5.times { Order.create! }
+
 # Shipped at times are randomly assigned.  What great randomizaiton I've done!
-os1 = Order.create!(shipped_at: Time.now + 21.minute)
-os2 = Order.create!(shipped_at: Time.now + 20.minute)
-os3 = Order.create!(shipped_at: Time.now + 3.minute)
-os4 = Order.create!(shipped_at: Time.now + 40.minute)
-os5 = Order.create!(shipped_at: Time.now + 5.minute)
+puts '***** Creating shipped Orders *****'
+5.times { Order.create!(shipped_at: Time.now+rand(10000)) }
+
+puts '***** Creating Widgets *****'
+10.times { Widget.create!(name: create_random_string, msrp: rand(100)) }
+
+puts "***** Done! #{Order.count} orders were created. #{Widget.count} Widgets were created. *****"
